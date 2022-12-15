7dea3edce862c4099044844913e17fb492bea20b
M-Wajid <wajidaman5@gmail.com>
2022-12-15 20:30:40 +0500

    final commit

--- routes/subscriberRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require ("express");
-const { addEmail } = require("../controllers/subscriberController");
-const subscriberRouter = express.Router();
-
-subscriberRouter.post("/addEmail", addEmail);
-
-module.exports = subscriberRouter;
